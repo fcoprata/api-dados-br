7bee9bca2f30da21855372c28c3e85807dcb3a48
Francisco Prata <fcopratan@gmail.com>
2024-04-29 16:33:26 -0300

    Refactor main.py to use environment variable for port configuration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
 import uvicorn
+import os
+from fastapi import FastAPI
 from models.cpf import Cpf
 from models.cnpj import Cnpj
 from models.gerador_cpf import Gerador_cpf
@@ -144,4 +145,5 @@ def buscador_cep(cep: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
